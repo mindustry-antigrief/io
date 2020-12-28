dc187c47b6ae3f0f4cb98038ff9e1131f35c6672
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-12-28 18:33:16 +0100

    spore too slow

--- core/src/mindustry/creeper/CreeperUtils.java
+++ core/src/mindustry/creeper/CreeperUtils.java
@@ -43,10 +43,10 @@ public class CreeperUtils {
     public static float minCreeper = 0.5f; // Minimum amount of creeper required for transfer
 
     public static BulletType sporeType = Bullets.artilleryDense;
-    public static float sporeMaxRangeMultiplier = 40f;
+    public static float sporeMaxRangeMultiplier = 30f;
     public static float sporeAmount = 30f;
     public static float sporeRadius = 3f;
-    public static float sporeSpeedMultiplier = 0.05f;
+    public static float sporeSpeedMultiplier = 0.15f;
     public static float sporeHealthMultiplier = 10f;
     public static float sporeTargetOffset = 256f;
 
