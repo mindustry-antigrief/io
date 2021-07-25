adaa62e9a8574e9be955e2d1f9f299c291e6dac9
Traqun <tranquillyunpleasant@gmail.com>
2021-07-25 23:47:10 +0500

    layered additive rings

--- core/src/mindustry/ai/formations/patterns/LayeredFormation.java
+++ core/src/mindustry/ai/formations/patterns/LayeredFormation.java
@@ -2,6 +2,7 @@ package mindustry.ai.formations.patterns;
 
 import arc.math.*;
 import arc.math.geom.*;
+import arc.util.*;
 import mindustry.ai.formations.*;
 
 public class LayeredFormation extends FormationPattern{
@@ -10,15 +11,11 @@ public class LayeredFormation extends FormationPattern{
 
     @Override
     public Vec3 calculateSlotLocation(Vec3 outLocation, int slotNumber){
-        if(slots > 8){
-            int rows = 1 + slots / 8;
-            int row = slotNumber % rows + 1;
-            float angle = (360f * slotNumber) / slots;
-            float radius = spacing * row * 2;
-            outLocation.set(Angles.trnsx(angle, radius), Angles.trnsy(angle, radius), angle);
-        }else if(slots > 1){
-            float angle = (360f * slotNumber) / slots;
-            float radius = spacing / (float)Math.sin(180f / slots * Mathf.degRad);
+        if(slots > 1){
+            int row = row(slotNumber);
+            int cap = Math.min(slots - 8 * row * (row - 1) / 2, row * 8);
+            float angle = (360f * (slotNumber % cap)) / cap;
+            float radius = spacing + spacing * row * 2;
             outLocation.set(Angles.trnsx(angle, radius), Angles.trnsy(angle, radius), angle);
         }else{
             outLocation.set(0, spacing * 1.1f, 360f * slotNumber);
@@ -29,4 +26,11 @@ public class LayeredFormation extends FormationPattern{
         return outLocation;
     }
 
+    private int row(int slot){
+        for(int i = 0; i < slots; i++){
+            if(slot - 8 * i * (i + 1) / 2 < 8 * (i + 1)) return i + 1;
+        }
+        // This should never get here but if it does its fine i guess
+        return 1;
+    }
 }
