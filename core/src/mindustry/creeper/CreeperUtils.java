13effa69426ee6cfcaa9df641f735d5bbaa98293
traqun <tranquillyunpleasant@gmail.com>
2021-11-11 03:40:44 +0500

    dont need this

--- core/src/mindustry/creeper/CreeperUtils.java
+++ core/src/mindustry/creeper/CreeperUtils.java
@@ -28,7 +28,6 @@ public class CreeperUtils{
     public static float creeperDamage = 0.1f; // Base creeper damage
     public static float damageEvaporationRate = 0.96f; // Creeper percentage that will remain upon damaging something
     public static float creeperUnitDamage = 2f;
-    public static float minCreeper = 0.99f; // Minimum amount of creeper required for transfer
 
     public static BulletType sporeType = Bullets.artilleryDense;
     public static float sporeMaxRangeMultiplier = 30f;
