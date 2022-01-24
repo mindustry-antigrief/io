d572611dc3945c08e11cadc4ae0c2fda06c6b51f
Tranquilly Unpleasant <tranquillyunpleasant@gmail.com>
2022-01-24 19:27:42 +0100

    refactor to type enum

--- core/src/mindustry/creeper/Emitter.java
+++ core/src/mindustry/creeper/Emitter.java
@@ -2,16 +2,28 @@ package mindustry.creeper;
 
 import arc.graphics.*;
 import arc.math.geom.*;
+import arc.util.*;
 import mindustry.content.*;
 import mindustry.gen.*;
+import mindustry.world.*;
+
+import java.util.*;
+
+import static mindustry.creeper.CreeperUtils.*;
 
 public class Emitter implements Position{
-    public int interval;
-    public int amt;
     public Building build;
+    public EmitterType type;
     public boolean nullified;
 
     protected int counter;
+
+    public static HashMap<Block, EmitterType> emitterTypes = new HashMap<>();
+
+    public Emitter(Building build){
+        this.build = build;
+        this.type = emitterTypes.get(build.block);
+    }
 
     // updates every interval in CreeperUtils
     public boolean update(){
@@ -20,11 +32,11 @@ public class Emitter implements Position{
 
         nullified = build.nullifyTimeout > 0f;
 
-        if (counter >= interval){
+        if(counter >= type.interval){
             counter = 0;
-            build.tile.getLinkedTiles(t -> {
-                t.creep = nullified ? Math.min(t.creep, 10) : t.creep + amt;
-            });
+            build.tile.getLinkedTiles(t ->
+            t.creep = nullified ? Math.min(t.creep, maxTileCreep) : t.creep + type.amt
+            );
         }
         counter++;
 
@@ -37,19 +49,17 @@ public class Emitter implements Position{
             Call.label("[red]*[] SUSPENDED [red]*[]", 1f, build.x, build.y);
             Call.effect(Fx.placeBlock, build.x, build.y, build.block.size, Color.yellow);
         }
-    }
-
-    public Emitter(int interval, int amt){
-        this.interval = interval;
-        this.amt = amt;
-    }
-
-    public Emitter(Building build){
-        this.build = build;
-
-        var ref = CreeperUtils.emitterBlocks.get(build.block);
-        this.interval = ref.interval;
-        this.amt = ref.amt;
+        if (build != null && build.tile != null && type.upgradeThreshold > 0 && build.tile.creep > 20){
+            Call.label(Strings.format("[green]*[white] UPGRADING []@% *[]", (int) (build.tile.creep * 100 / type.upgradeThreshold)), 1f, build.x, build.y);
+            if (build.tile.creep > type.upgradeThreshold){
+                EmitterType next = type.getNext();
+                if(next != null){
+                    build.tile.setNet(next.block, creeperTeam, 0);
+                    this.build = build.tile.build;
+                    this.type = next;
+                }
+            }
+        }
     }
 
     @Override
@@ -60,5 +70,40 @@ public class Emitter implements Position{
     @Override
     public float getY(){
         return build.y;
+    }
+
+    public static void init(){
+        emitterTypes.put(Blocks.coreShard, EmitterType.shard);
+        emitterTypes.put(Blocks.coreFoundation, EmitterType.foundation);
+        emitterTypes.put(Blocks.coreNucleus, EmitterType.nucleus);
+    }
+
+    public enum EmitterType{
+        shard(3, 30, 1, 30, Blocks.coreShard),
+        foundation(5, 20, 2, 3000, Blocks.coreFoundation),
+        nucleus(7, 15, 3, -1, Blocks.coreNucleus);
+
+        public final int amt;
+        public final int level;
+        public final int interval;
+        public final int upgradeThreshold;
+        public final Block block;
+
+        EmitterType(int amt, int interval, int level, int upgradeThreshold, Block block){
+            this.amt = amt;
+            this.level = level;
+            this.block = block;
+            this.interval = interval;
+            this.upgradeThreshold = upgradeThreshold;
+        }
+
+        public EmitterType getNext(){
+            for(EmitterType t : values()){
+                if(t.level == (level + 1)){
+                    return t;
+                }
+            }
+            return null;
+        }
     }
 }
